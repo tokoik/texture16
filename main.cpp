29b6c35179cdc7ccfc92927454de5342937bcbf2
Kohe Tokoi <tokoik@gmail.com>
2026-05-09 10:39:17 +0900

    refactor: clean up GLUT includes, suppress macOS deprecation warnings, and add project README

--- main.cpp
+++ main.cpp
@@ -1,16 +1,22 @@
-﻿#include <stdio.h>
+﻿#if defined(__APPLE__)
+#  define GL_SILENCE_DEPRECATION
+#  include <GLUT/glut.h>
+#  include <OpenGL/glext.h>
+#else
+#  if defined(_WIN32)
+//#    pragma comment(linker, "/subsystem:\"windows\" /entry:\"mainCRTStartup\"")
+#    define _USE_MATH_DEFINES
+#    define _CRT_SECURE_NO_WARNINGS
+#  endif
+#  include <GL/glut.h>
+#  include <GL/glext.h>
+#  if defined(_WIN32)
+PFNGLACTIVETEXTUREPROC glActiveTexture;
+#  endif
+#endif
+#include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#if defined(_WIN32)
-//#  pragma comment(linker, "/subsystem:\"windows\" /entry:\"mainCRTStartup\"")
-#  include "glut.h"
-#  include "glext.h"
-PFNGLACTIVETEXTUREPROC glActiveTexture;
-#elif defined(__APPLE__) || defined(MACOSX)
-#  include <GLUT/glut.h>
-#else
-#  include <GL/glut.h>
-#endif
 
 /* トラックボール処理用関数の宣言 */
 #include "trackball.h"
@@ -21,58 +27,73 @@ PFNGLACTIVETEXTUREPROC glActiveTexture;
 /*
 ** 光源
 */
-static const GLfloat lightpos[] = { 0.0, 0.0, 1.0, 0.0 }; /* 位置　　　 */
-static const GLfloat lightcol[] = { 1.0, 1.0, 1.0, 1.0 }; /* 直接光強度 */
-static const GLfloat lightamb[] = { 0.1, 0.1, 0.1, 1.0 }; /* 環境光強度 */
+static const GLfloat lightpos[] = { 0.0f, 0.0f, 1.0f, 0.0f }; /* 位置　　　 */
+static const GLfloat lightcol[] = { 1.0f, 1.0f, 1.0f, 1.0f }; /* 直接光強度 */
+static const GLfloat lightamb[] = { 0.1f, 0.1f, 0.1f, 1.0f }; /* 環境光強度 */
 
 /*
 ** テクスチャ
 */
-#define TEXWIDTH  256                      /* テクスチャの幅　　　 */
-#define TEXHEIGHT 256                      /* テクスチャの高さ　　 */
-static const char texture1[] = "dot.raw";  /* テクスチャファイル名 */
+#define TEXWIDTH  256                               /* テクスチャの幅　　　 */
+#define TEXHEIGHT 256                               /* テクスチャの高さ　　 */
+static const char texture_file[] = "dot.raw";       /* テクスチャファイル名 */
+static GLuint texname[2];                           /* テクスチャ名（番号） */
 
 /*
 ** 初期化
 */
 static void init()
 {
+  /* テクスチャ画像はワード単位に詰め込まれている */
+  glPixelStorei(GL_UNPACK_ALIGNMENT, 4);
+
   /* テクスチャの読み込みに使う配列 */
   GLubyte texture[TEXHEIGHT * TEXWIDTH * 4];
   FILE *fp;
-  
-  /* テクスチャ画像はワード単位に詰め込まれている */
-  glPixelStorei(GL_UNPACK_ALIGNMENT, 4);
-  
+
   /* テクスチャ画像の読み込み */
-  if ((fp = fopen(texture1, "rb")) != NULL) {
+  if ((fp = fopen(texture_file, "rb")) != NULL) {
     fread(texture, sizeof texture, 1, fp);
     fclose(fp);
   }
-  
-  /* テクスチャの割り当て */
-  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, TEXWIDTH, TEXHEIGHT, 0,
-    GL_RGBA, GL_UNSIGNED_BYTE, texture);
-  
-  /* テクスチャを拡大・縮小する方法の指定 */
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-  
-  /* テクスチャの繰り返し方法の指定 */
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
-  
-  /* テクスチャ環境 */
-  glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
-  
+  else {
+    perror(texture_file);
+  }
+
 #if defined(_WIN32)
   glActiveTexture =
     (PFNGLACTIVETEXTUREPROC)wglGetProcAddress("glActiveTexture");
 #endif
-  
-  /* テクスチャユニット１に切り替える */
+
+  /* テクスチャ名を２つ作る */
+  glGenTextures(2, texname);
+
+  /* １つ目のテクスチャ名には２次元テクスチャを割り当てる */
+  glActiveTexture(GL_TEXTURE0);
+  glBindTexture(GL_TEXTURE_2D, texname[0]);
+
+  /* テクスチャの割り当て */
+  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, TEXWIDTH, TEXHEIGHT, 0,
+    GL_RGBA, GL_UNSIGNED_BYTE, texture);
+
+  /* テクスチャを拡大・縮小する方法の指定 */
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+
+  /* テクスチャの繰り返し方法の指定 */
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+  /* 無名テクスチャに戻しておく */
+  glBindTexture(GL_TEXTURE_2D, 0);
+
+  /* テクスチャユニット０のテクスチャ環境 */
+  glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
+
+  /* ２つ目のテクスチャにはキューブマップを割り当てる*/
   glActiveTexture(GL_TEXTURE1);
-  
+  glBindTexture(GL_TEXTURE_CUBE_MAP, texname[1]);
+
   for (int i = 0; i < 6; ++i) {
     /* テクスチャファイル名 */
     static const char *textures[] = {
@@ -92,7 +113,7 @@ static void init()
       GL_TEXTURE_CUBE_MAP_NEGATIVE_X,
       GL_TEXTURE_CUBE_MAP_POSITIVE_Y,
     };
-    
+
     if ((fp = fopen(textures[i], "rb")) != NULL) {
       /* テクスチャ画像の読み込み */
       fread(texture, 128 * 128 * 4, 1, fp);
@@ -103,39 +124,39 @@ static void init()
         GL_RGBA, GL_UNSIGNED_BYTE, texture);
     }
   }
-  
+
   /* テクスチャを拡大・縮小する方法の指定 */
   glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
   glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-  
+
   /* テクスチャの繰り返し方法の指定 */
   glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP);
   glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP);
-  
+
+  /* キューブマッピング用のテクスチャ座標を生成する */
+  glTexGeni(GL_S, GL_TEXTURE_GEN_MODE, GL_REFLECTION_MAP);
+  glTexGeni(GL_T, GL_TEXTURE_GEN_MODE, GL_REFLECTION_MAP);
+  glTexGeni(GL_R, GL_TEXTURE_GEN_MODE, GL_REFLECTION_MAP);
+
+  /* 無名テクスチャに戻しておく */
+  glBindTexture(GL_TEXTURE_CUBE_MAP, 0);
+
   /* テクスチャユニット１のテクスチャ環境 */
   glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_COMBINE);
   glTexEnvi(GL_TEXTURE_ENV, GL_COMBINE_RGB, GL_INTERPOLATE);
 #if 0
-  static const GLfloat blend[] = { 1.0, 1.0, 1.0, 0.5 };
+  static const GLfloat blend[] = { 1.0f, 1.0f, 1.0f, 0.5f };
   glTexEnvfv(GL_TEXTURE_ENV, GL_TEXTURE_ENV_COLOR, blend);
 #else
   glTexEnvi(GL_TEXTURE_ENV, GL_SOURCE2_RGB, GL_PREVIOUS);
   glTexEnvi(GL_TEXTURE_ENV, GL_OPERAND2_RGB, GL_SRC_ALPHA);
 #endif
-  
-  /* キューブマッピング用のテクスチャ座標を生成する */
-  glTexGeni(GL_S, GL_TEXTURE_GEN_MODE, GL_REFLECTION_MAP);
-  glTexGeni(GL_T, GL_TEXTURE_GEN_MODE, GL_REFLECTION_MAP);
-  glTexGeni(GL_R, GL_TEXTURE_GEN_MODE, GL_REFLECTION_MAP);
-  
-  /* テクスチャユニット０に戻す */
-  glActiveTexture(GL_TEXTURE0);
-  
+
   /* 初期設定 */
-  glClearColor(0.3, 0.3, 1.0, 0.0);
+  glClearColor(0.3f, 0.3f, 1.0f, 0.0f);
   glEnable(GL_DEPTH_TEST);
   glDisable(GL_CULL_FACE);
-  
+
   /* 光源の初期設定 */
   glEnable(GL_LIGHTING);
   glEnable(GL_LIGHT0);
@@ -149,42 +170,43 @@ static void init()
 */
 static void scene()
 {
-  static const GLfloat color[] = { 1.0f, 1.0f, 1.0f, 1.0f };  /* 材質 (色) */
-  
+  static const GLfloat color[] = { 1.0f, 1.0f, 1.0f, 1.0f };   /* 材質 (色) */
+
   /* 材質の設定 */
   glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, color);
-  
+
+  /* テクスチャユニット０を有効にする */
+  glActiveTexture(GL_TEXTURE0);
+  glBindTexture(GL_TEXTURE_2D, texname[0]);
+
   /* テクスチャマッピング開始 */
   glEnable(GL_TEXTURE_2D);
-  
-  /* テクスチャユニット１に切り替える */
+
+  /* テクスチャユニット１を有効にする */
   glActiveTexture(GL_TEXTURE1);
-  
+  glBindTexture(GL_TEXTURE_CUBE_MAP, texname[1]);
+
   /* キューブマッピング開始 */
   glEnable(GL_TEXTURE_CUBE_MAP);
   glEnable(GL_TEXTURE_GEN_S);
   glEnable(GL_TEXTURE_GEN_T);
   glEnable(GL_TEXTURE_GEN_R);
-  
+
   /* トラックボール処理による回転 */
   glMultMatrixd(trackballRotation());
 
   /* 箱を描く */
   box(1.0, 1.0, 1.0);
-  
+
   /* キューブマッピング終了 */
   glDisable(GL_TEXTURE_GEN_S);
   glDisable(GL_TEXTURE_GEN_T);
   glDisable(GL_TEXTURE_GEN_R);
   glDisable(GL_TEXTURE_CUBE_MAP);
-  
-  /* テクスチャユニット０に戻す */
-  glActiveTexture(GL_TEXTURE0);
-  
+
   /* テクスチャマッピング終了 */
   glDisable(GL_TEXTURE_2D);
 }
-
 
 /****************************
 ** GLUT のコールバック関数 **
@@ -192,22 +214,23 @@ static void scene()
 
 static void display()
 {
-  /* 画面クリア */
-  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-  
   /* モデルビュー変換行列の設定 */
   glMatrixMode(GL_MODELVIEW);
   glLoadIdentity();
-  
+
   /* 光源の位置を設定 */
   glLightfv(GL_LIGHT0, GL_POSITION, lightpos);
-  
+
   /* 視点の移動（物体の方を奥に移動）*/
   glTranslated(0.0, 0.0, -5.0);
-  
+  //gluLookAt(1.5, 2.0, 2.5, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+
+  /* 画面クリア */
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
   /* シーンの描画 */
   scene();
-  
+
   /* ダブルバッファリング */
   glutSwapBuffers();
 }
@@ -216,13 +239,13 @@ static void resize(int w, int h)
 {
   /* トラックボールする範囲 */
   trackballRegion(w, h);
-  
+
   /* ウィンドウ全体をビューポートにする */
   glViewport(0, 0, w, h);
-  
+
   /* 透視変換行列の指定 */
   glMatrixMode(GL_PROJECTION);
-  
+
   /* 透視変換行列の初期化 */
   glLoadIdentity();
   gluPerspective(40.0, (double)w / (double)h, 0.1, 10.0);
